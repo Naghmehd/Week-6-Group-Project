4e2ec7dcdb96ceccb1ebe992ababfe5082e11bdf
JaredPowers <jarpow44@gmail.com>
2016-04-08 15:18:45 -0500

    set up seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,29 +6,13 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-movies = ["Batman", "grenre","Spiderman","genre","Amalie","genre"]
+movies = [
+  ["Batman", "grenre"],
+  ["Spiderman","genre"],
+  ["Amalie","genre"]
+]
 
-
-movie = Movie.create(title: movies.shift,
-                    genre: movies.shift
-                    )
-
-20.times do
-  user = User.create!(name: Faker::Name.name,
-                      username: Faker::Internet.user_name,
-                      password: Faker::Internet.password,
-                      )
-                  end
-
-  5.times do
-    movie = Movie.create!(title: movies.pop
-                          user_id: rand(1..20))
-
-      rand(1..5).times do
-                    Upvote.create!(movie_id: movie.id)
-
-          rand(1..5).times do
-                    Downvote.create!(movie_id: movie.id)
-          end
-      end
-  end
+movies.each do |title, genre|
+  Movie.create(title: title
+              genre: genre)
+end
