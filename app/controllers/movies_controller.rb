300728b583fd99b1823993c1797a8ce83fa034d2
JaredPowers <jarpow44@gmail.com>
2016-04-08 15:04:46 -0500

    added movies and users controllers

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,2 +1,43 @@
 class MoviesController < ApplicationController
+
+  def index
+    @Movie.Movie.all
+  end
+
+  def show
+    @Movie = set_Movie
+  end
+
+  def new
+    @Movie = Movie.new
+  end
+
+  def create
+    @Movie = Movie.new
+    @Movie.save
+  end
+
+  def edit
+    @Movie = set_Movie
+  end
+
+  def update
+    Movie.update(params[:id], Movie_params)
+  end
+
+  def destroy
+    @Movie = set_Movie
+    @Movie.destroy
+  end
+
+private
+
+  def set_Movie
+    @Movie = Movie.find(params[:id])
+  end
+
+  def Movie_params
+    params.require(:Movie).permit(:Moviename)
+  end
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,43 @@
 class UsersController < ApplicationController
+
+  def index
+    @user.User.all
+  end
+
+  def show
+    @user = set_user
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new
+    @user.save
+  end
+
+  def edit
+    @user = set_user
+  end
+
+  def update
+    User.update(params[:id], user_params)
+  end
+
+  def destroy
+    @user = set_user
+    @user.destroy
+  end
+
+private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:username)
+  end
+
 end
