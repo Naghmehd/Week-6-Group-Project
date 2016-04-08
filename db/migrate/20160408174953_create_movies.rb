d576a4044334beea0ceae3c0b2fe82dacd000bb1
Naghmeh Shirazi <naghmehd@gmail.com>
2016-04-08 14:17:26 -0500

    added genre to the movies

--- db/migrate/20160408174953_create_movies.rb
+++ db/migrate/20160408174953_create_movies.rb
@@ -1,7 +1,8 @@
 class CreateMovies < ActiveRecord::Migration
   def change
     create_table :movies do |t|
-      t.title :string, null: false
+      t.string :title, null: false
+      t.string :genre, null: false
       t.timestamps null: false
     end
   end
