9c1df2f9e16412d8d62a680b5f592b40c8ed9ac2
Naghmeh Shirazi <naghmehd@gmail.com>
2016-04-09 09:22:32 -0500

    added api for upvote controller and rename the file

---
+++ app/controllers/API/Upvote_controller.rb
@@ -0,0 +1,17 @@
+class Api::Upvotes_Controller < ApplicationController
+  protect_from_forgery with: :null_session
+
+  def index
+    @upvote = Vote.all
+    render json: @upvote = Upvote.all
+  end
+
+  def create_upvote
+    @upvote = Upvote.new
+    @upvote.movie = movie.find(params[:movie_id])
+    @upvote.save
+    
+    render json:
+  end
+
+end
