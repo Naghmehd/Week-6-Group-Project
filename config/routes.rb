9603978a6e42a7fd6271296e44bd294c39e89caf
Naghmeh Shirazi <naghmehd@gmail.com>
2016-04-09 16:14:06 -0500

    edited the router for upvotes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   namespace :api do
     resources :movies do
-      post '/upvotes/:link_id', to: 'upvotes#create_upvote', as: 'upvote_link'
+      post '/upvotes/:movie_id', to: 'upvotes#create_upvote', as: 'upvote_movies'
       resources :upvotes
     end
   end
