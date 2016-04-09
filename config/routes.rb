b33e126ca59d0f387f346b5829e1526027380d85
Naghmeh Shirazi <naghmehd@gmail.com>
2016-04-09 14:06:09 -0500

    removed users from apicontroller controller routes and seed

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,6 @@
 Rails.application.routes.draw do
 
   namespace :api do
-    get '/sign_in', to: 'sessions#sign_in', as: 'sign_in'
-    post '/sign_in', to: 'sessions#create'
-    get '/sign_out', to: 'sessions#sign_out', as: 'sign_out'
-    delete '/sign_out', to: 'sessions#destroy'
-    resources :users
-
     resources :movies do
       resources :upvotes
     end
--- db/migrate/20160408180918_create_downvotes.rb
+++
@@ -1,8 +0,0 @@
-class CreateDownvotes < ActiveRecord::Migration
-  def change
-    create_table :downvotes do |t|
-      t.integer :movie_id, null: false
-      t.timestamps null: false
-    end
-  end
-end
